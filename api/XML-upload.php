242504dccb663dcb509cdee6c7df85e66639bf5f
Ryan Ammons <ryan.ammons@celticwolf.com>
2011-12-19 21:59:13 -0500

    Modified API to accept a POST variable named 'xml' rather than the original specification for an XML file attachment. Also, modified XML-upload.php as a test POST form with dummy XML order data.

--- api/XML-upload.php
+++ api/XML-upload.php
@@ -1,19 +1,14 @@
 <center>
-<h2>POST Mock XML Order File to 'course-purchased.php'</h2>
+<h2>POST Mock XML Order to 'course-purchased.php'</h2>
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="course-purchased.php" method="POST">
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
     <!-- Name of input element determines name in $_FILES array -->
-    Send this file: <input name="file" type="file"/>
+    XML POST Data: <textarea name="xml" id="xml">
+	&gt; &lt;?xml version=\&quot;1.0\&quot; encoding=\&quot;UTF-8\&quot; ?&gt; &lt;CreateOrder xmlns=\&quot;\&quot;&gt;&lt;Timestamp&gt;2011-12-16T20:51:00Z&lt;/Timestamp&gt;&lt;Store&gt;https://www.californiaeducationconnection.com&lt;/Store&gt;&lt;OrderID&gt;24644&lt;/OrderID&gt;&lt;InvoiceNumber&gt;3ON2520&lt;/InvoiceNumber&gt;&lt;Date&gt;12/14/2011&lt;/Date&gt;&lt;Student&gt;&lt;FirstName&gt;Betty&lt;/FirstName&gt;&lt;LastName&gt;Shaw&lt;/LastName&gt;&lt;Email&gt;twokos@aol.com&lt;/Email&gt;&lt;Address&gt;11424 Oneonta Knoll&lt;/Address&gt;&lt;Address2&gt;&lt;/Address2&gt;&lt;City&gt;South Pasadena&lt;/City&gt;&lt;ZipCode&gt;91030&lt;/ZipCode&gt;&lt;StateCode&gt;CA&lt;/StateCode&gt;&lt;CountryCode&gt;US&lt;/CountryCode&gt;&lt;Phone&gt;951-990-1599&lt;/Phone&gt;&lt;Company&gt;&lt;/Company&gt;&lt;IP&gt;166.250.64.186&lt;/IP&gt;&lt;/Student&gt;&lt;Course&gt;&lt;CourseID&gt;CEAPPIND&lt;/CourseID&gt;&lt;CourseName&gt;CE Application Fee Individual RUSH FEE: RUSH FEE&lt;/CourseName&gt;&lt;CoursePrice&gt;35&lt;/CoursePrice&gt;&lt;/Course&gt;&lt;/CreateOrder&gt;
+	</textarea>
     <input type="submit" value="Send File" />
 </form>
-<p>
-    <a href="order.xml">Download the mock XML order file</a> in order to upload it...
-    <br>
-    <h4>
-    (Right-click link, Save As...to your Desktop, then browse for/upload it above.)
-    </h4>
-</p>
 
 </center>
--- api/course-purchased.php
+++ api/course-purchased.php
@@ -7,76 +7,32 @@ $br="<br>";
 $hr="<hr>";
 
 //POST Authenticity check
-// Ensure that californiaeducationconnection.com server IP is in fact
-// the origin of the POST form action.
-
-//Static IP for californiaeducationconnection.com
-//$authorized_ip = '174.37.45.113';
+// Ensure that the origin of the POST form action is *.3dcartstores.com.
 $authorized_host = "3dcartstores.com";
-
-//$remote_host = $_SERVER['REMOTE_HOST'];
-//$server_name = $_SERVER['SERVER_NAME'];
-
 $remote_ip = $_SERVER['REMOTE_ADDR'];
-
 $hostname = gethostbyaddr($remote_ip);
-
 $regexPattern = "/".$authorized_host."$/";
 
 //$hostname="3dcartstores.com";
-
-//mail("ryan@rammons.net","Script Accessed by ".$hostname,"","From:admin@learningfox.com");
 if (0==preg_match($regexPattern,$hostname))
     exit( "Not authorized." );
 else
     echo "Authorized.".$br.$hr;
 
-//mail("ryan@rammons.net","Authorized Access by ".$hostname,"","From:admin@learningfox.com");
-
-$formData = print_r($_REQUEST, true);
-
-//Verify that the XML file was saved without error before continuing
-if ($_FILES["file"]["error"] > 0)  {
-  	mail("ryan@rammons.net","Error","Error: ".$_FILES["file"]["error"],"From:admin@learningfox.com");
-	echo "Error: ".$_FILES["file"]["error"].$br;
-	die();
-}
-/*else
-  {
-  echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-  echo "Type: " . $_FILES["file"]["type"] . "<br />";
-  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
-  echo "<br><br>";
-  }*/
-
-//Retrieve POSTed temporary XML file
-
-$file = $_FILES["file"]["tmp_name"];
-
-$numFiles=0;
-foreach($_FILES as $eachFile)
-{
-     if($eachFile['size'] > 0)
-        $numFiles++;
-}
-
-
-//Get the file contents as a string, then HTML-encode ampersands
 //                              (SimpleXML chokes on non-encoded &'s...)
-$data = ereg_replace("& ", "&amp; ", file_get_contents($file));
+//$data = ereg_replace("& ", "&amp; ", file_get_contents($file));
 
-mail("ryan@rammons.net","Begin parsing XML",$data." - number of files: ".$numFiles."\n\r".$formData,"From:admin@learningfox.com");
+//Get the POST variable 'xml' and HTML-dencode
+$data = html_entity_decode($_POST['xml'], ENT_QUOTES, 'UTF-8');
+
+mail("ryan@rammons.net","Begin parsing XML - XML POST Data Field",htmlentities($data),"From:admin@learningfox.com");
 
 //Parse the XML file's contents
-
 try {
 	$order = new SimpleXMLElement($data);
 } catch (Exception $e) {
     mail("ryan@rammons.net","SimpleXMLElement parse excetion","Caught exception: ".$e->getMessage(),"From:admin@learningfox.com"); 
 }
-
-mail("ryan@rammons.net","Parse of XML successful","","From:admin@learningfox.com");
 
 $paymentData['store'] = $order->Store;
 $paymentData['orderid'] = $order->OrderID;
@@ -101,11 +57,7 @@ $paymentData['courseprice'] = $order->Course->CoursePrice;
     echo $key.": ".$value.$br;
 }*/
 
-//echo $hr;
-
-
 mail("ryan@rammons.net","Order info",implode(",",$paymentData),"From:admin@learningfox.com");
-
 
 //Write values to the LMS database and generate/send credentials to user
 update_LMS($paymentData, $dir_usercourselist, $paymentData['courseid']);
