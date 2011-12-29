e928a4bf52733359796273edaaee828ae3df29e9
Ryan Ammons <ryan.ammons@celticwolf.com>
2011-12-29 17:58:31 -0500

    Synch of /admin folder to Cosmos-Content production.

--- admin/conn.php
+++ admin/conn.php
@@ -5,7 +5,7 @@ $db = mysql_connect("localhost", "root", "") or die(mysql_error());
 $dbName = mysql_select_db("lms") or die(mysql_error());
 */
 
-$db = mysql_connect("p50mysql107.secureserver.net", "lmsv1db", "Technologies14") or die(mysql_error());
-$dbName = mysql_select_db("lmsv1db") or die(mysql_error());
+$db = mysql_connect("cosmoscourseslms.db.8685149.hostedresource.com", "cosmoscourseslms", "K3RgmwVqh6") or die(mysql_error());
+$dbName = mysql_select_db("cosmoscourseslms") or die(mysql_error());
 
 ?>
--- admin/includes/connection.php
+++ admin/includes/connection.php
@@ -3,8 +3,6 @@ session_start();
 
 //$db = mysql_connect("localhost", "root", "") or die(mysql_error());
 //$dbName = mysql_select_db("rcheck") or die(mysql_error());
-//$db = mysql_connect("localhost", "iloilotr_kenneth", "htennek7") or die(mysql_error());
-//$dbName = mysql_select_db("iloilotr_rcheck") or die(mysql_error());
-$db = mysql_connect("p50mysql107.secureserver.net", "lmsv1db", "Technologies14") or die(mysql_error());
-$dbName = mysql_select_db("lmsv1db") or die(mysql_error());
+$db = mysql_connect("localhost", "iloilotr_kenneth", "htennek7") or die(mysql_error());
+$dbName = mysql_select_db("iloilotr_rcheck") or die(mysql_error());
 ?>
