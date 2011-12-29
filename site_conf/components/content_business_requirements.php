eb92683900482e85d935c11382c5b3137623c42f
Ryan Ammons <ryan.ammons@celticwolf.com>
2011-12-29 16:58:40 -0500

    Synch of /site_conf with production.

---
+++ site_conf/components/content_business_requirements.php
@@ -0,0 +1,7 @@
+<div align="center" style="width:800px;">
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="4">
+  <TR>
+    <TD align="center"><strong><FONT color="#FF0000">Page under Construction</FONT></strong></TD>
+  </TR>
+</TABLE>
+</div>
