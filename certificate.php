8f37ea3593a16c165d557fbc5e9fc475090d09d8
Ryan Ammons <ryan@ammonsdatasolutions.com>
2014-04-30 18:56:21 -0400

    Changed how interface generates PDF for certificate.

--- certificate.php
+++ certificate.php
@@ -14,7 +14,6 @@ $userid = $_GET["userid"];
 $qry = "SELECT * FROM course_history, course, students WHERE course_history.user_ID = '$userid' AND
 course_history.course_ID = '$course_ID' AND course.id = '$course_ID' AND students.ID = '$userid'";
 $db->query($qry);
-//echo "[$qry]";
 
 while($db->getRows()) {
     $course_info['course_status'] = ucfirst($db->row("course_status"));
@@ -30,35 +29,47 @@ while($db->getRows()) {
 $db->close();
 $db = null;
 
+$logo = "site_conf/images/certificate_logo.png";
+
 $html ='<!DOCTYPE html>
 <html>
 <head>
     <title>Certificate of Completion</title>
+    <style>
+    .padded{line-height:200%;display:block}
+    </style>
 </head>
 <body>
-<div style="width:850px; height:600px; padding:20px; text-align:center; border: 10px solid #787878">
-    <div style="width:800px; height:550px; padding:20px; text-align:center; border: 5px solid #787878">
-        <img src="site_conf/images/certificate_logo.png"/>
-        <br><br>
-        <span style="font-size:50px; font-weight:bold">Certificate of Completion</span>
-        <br><br>
-        <span style="font-size:25px"><i>This is to certify that</i></span>
-        <br><br>
-        <span style="font-size:30px"><b>'.$course_info["first_name"].' '.$course_info["last_name"];
-?></b></span><br/><br/>
-        <span style="font-size:25px"><i>has completed the course</i></span> <br/><br/>
-        <span style="font-size:30px"><?=$course_info["course_name"];?></span>
-        <br/><br/>
-        <span style="font-size:20px">with a score of <b><?=$course_info["score_raw"];?>%</b></span>
-        <br/><br/><br/>
-        <span style="font-size:22px"><i>dated</i></span><br>
-        <span style="font-size:25px"><?=$course_info["completion_date"];?></span>
+<div style="width:950px; height:600px; padding:20px; text-align:center; border: 10px solid #787878">
+    <div style="width:900px; height:550px; padding:20px; text-align:center; border: 5px solid #787878">
+        <img src="'.$logo.'"/>
+        <br>
+        <span style="font-size:50px; font-weight:bold;">Certificate of Completion</span><br>
+        <span style="font-size:25px;"><i>This is to certify that</i></span><br>
+        <br>
+        <span style="font-size:30px; font-weight:bold;">'.$course_info["first_name"].' '.$course_info["last_name"].'</span><br>
+        <br>
+        <span style="font-size:25px"><i>has completed the course</i></span><br>
+        <br>
+        <span style="font-size:30px">'.$course_info["course_name"].'</span><br>
+        <br>
+        <span style="font-size:20px">with a score of <b>'.$course_info["score_raw"].'%</b></span><br>
+        <br>
+        <span style="font-size:20px"><i>dated</i></span> &nbsp;&nbsp;
+        <span style="font-size:20px">'.$course_info["completion_date"].'</span>
     </div>
 </div>
 </body>
 </html>';
 
-$mpdf=new mPDF();
-$mpdf->WriteHTML($html);
-$mpdf->Output();
-exit;
+if ($_GET['pdf']=='yes'){
+    $mpdf=new mPDF();
+    $mpdf->AddPage("L");
+    $mpdf->WriteHTML($html);
+    $mpdf->Output();
+    exit;
+}
+else{
+    echo $html;
+    exit;
+}
